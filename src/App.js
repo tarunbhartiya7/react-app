23e9bfd46df946a9aa30ea168315dd2d71f28206
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2020-12-13 16:34:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and deploy using jenkins
+          Learn React
         </a>
       </header>
     </div>
