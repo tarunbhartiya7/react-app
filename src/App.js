d3e7827285927fb1b209a4b96fbf0be0a65c3a6b
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2020-12-15 16:31:12 +0530

    Add blue ocean text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and Jenkins Blue Ocean
         </a>
       </header>
     </div>
