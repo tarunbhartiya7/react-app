44e33af73eed076e13b504b79a4597761ed0f373
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2020-12-15 15:43:23 +0530

    Added new changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and Jenkins
+          Learn React
         </a>
       </header>
     </div>
